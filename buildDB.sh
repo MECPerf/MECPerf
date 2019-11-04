b38e8c26361f0b8e37163e8c050702f01062c0ef
chiara.caiazza <chiara.caiazza@gmail.com>
2019-11-04 15:44:19 +0100

    script updated

--- buildDB.sh
+++ buildDB.sh
@@ -1,33 +1,46 @@
 #!/bin/bash
 
-REMOTE_PASS=$1
-ROOT_USER_PASS=$2
+if [[ $# -ne 6 ]]; then
+    echo "./buildDB sourceUser@sourceAddress sourcePassword destUsername destAddress destPassword  mysqlDestRootPassword"
+    exit 1
+fi
+
+SOURCE_USER=$1
+SOURCE_PASSWORD=$2
+REMOTE_USERNAME=$3
+REMOTE_ADDRESS=$4
+REMOTE_PASSWORD=$5
+MYSQL_ROOT_PASSWORD=$6
+
 USER_DB="MECPerf"
 PW_DB="password"
 DB_NAME="MECPerf"
 
-sshpass -p $REMOTE_PASS ssh ubuntu@131.114.73.3 <<EOF
+echo "sshpass -p $SOURCE_PASSWORD ssh $SOURCE_USER <<EOF"
+sshpass -p $SOURCE_PASSWORD ssh $SOURCE_USER <<EOF
     echo "building DB..."
     mysqldump --user=$USER_DB --password=$PW_DB --no-data $DB_NAME > MECPerf.sql
     echo "Database schema create with no data"
     echo "N.B: AUTO_INCREMENT in Test Table is not resetted"
-    sshpass -p $REMOTE_PASS scp MECPerf.sql ubuntu@131.114.73.2:/home/ubuntu
+
+    echo "sshpass -p $REMOTE_PASSWORD scp MECPerf.sql $REMOTE_USERNAME@$REMOTE_ADDRESS:/home/$REMOTE_USERNAME"
+    sshpass -p $REMOTE_PASSWORD scp MECPerf.sql $REMOTE_USERNAME@$REMOTE_ADDRESS:/home/$REMOTE_USERNAME
 EOF
 
-
-sshpass -p $REMOTE_PASS ssh ubuntu@131.114.73.2 <<EOF
-echo "mysql --user=root --password=$2 "
-mysql --user=root --password=$2 <<EOF2
-CREATE USER '$USER_DB'@'localhost' IDENTIFIED BY '$PW_DB';
+echo "sshpass -p $REMOTE_PASSWORD ssh $REMOTE_USERNAME@$REMOTE_ADDRESS <<EOF"
+sshpass -p $REMOTE_PASSWORD ssh $REMOTE_USERNAME@$REMOTE_ADDRESS <<EOF
+echo "mysql --user=root --password=$MYSQL_ROOT_PASSWORD "
+mysql --user=root --password=$MYSQL_ROOT_PASSWORD <<EOF2
+CREATE USER IF NOT EXISTS '$USER_DB'@'localhost' IDENTIFIED BY '$PW_DB';
 CREATE DATABASE  IF NOT EXISTS MECPerf;
 exit
 EOF2
 
 echo "importing MECPerf db"
-echo "mysql --user=root --password=$2 MECPerf < MECPerf.sql"
-mysql --user=root --password=$2  MECPerf < MECPerf.sql
+echo "mysql --user=root --password=$MYSQL_ROOT_PASSWORD MECPerf < MECPerf.sql"
+mysql --user=root --password=$MYSQL_ROOT_PASSWORD  MECPerf < MECPerf.sql
 
-mysql --user=root --password=$2 << EOF3
+mysql --user=root --password=$MYSQL_ROOT_PASSWORD << EOF3
 USE MECPerf;
 ALTER TABLE Test AUTO_INCREMENT=1;
 GRANT ALL PRIVILEGES ON MECPerf.* to 'MECPerf'@'localhost';
--- clearDB.sh
+++ clearDB.sh
@@ -1,28 +1,53 @@
 #!/bin/bash
 
-PASS=$"ve20ac19"
+if [[ ( $# -eq 1  && "$1" != 'localhost' ) ]]; then
+    echo "./clearDB remoteAddress [remotePassword] "
+    echo "./clearDB localhost "
+    
+    exit 1
+else
+    if [[ ( "$1" != 'localhost' && $# -ne 2 ) ]]; then
+        echo "./clearDB remoteAddress [remotePassword] "
+        echo "./clearDB localhost "
+        
+        exit 1
+    fi
+fi
+
+
+REMOTE_ADDRESS=$1
+REMOTE_PASS=$2
+
 USER_DB=$"MECPerf"
 PW_DB=$"password"
 NAME_DB=$"MECPerf"
 
-sshpass -p $PASS ssh ubuntu@131.114.73.3 <<EOF
+  
+if [ $# -eq 1 ] && [ "$1" = 'localhost' ]; then
+    echo "(localhost)clearing DB..."
+  
+    mysql --user=$USER_DB --password=$PW_DB $NAME_DB <<SQL
+        TRUNCATE RttMeasure;
+        TRUNCATE BandwidthMeasure;
+        DELETE FROM Test WHERE ID >= 1;
+        ALTER TABLE Test AUTO_INCREMENT = 1;
+SQL
+    
+    echo "DB cleared with success!"
 
-echo "clearing DB..."
+else
+    sshpass -p $REMOTE_PASS ssh ubuntu@$REMOTE_ADDRESS <<EOF
 
-mysql --user=$USER_DB --password=$PW_DB $NAME_DB <<SQL
+        echo "($REMOTE_ADDRESS)clearing DB..."
 
-TRUNCATE RttMeasure;
-
-TRUNCATE BandwidthMeasure;
-
-DELETE
-FROM Test
-WHERE ID >= 1;
-
-ALTER TABLE Test AUTO_INCREMENT = 1;
+        mysql --user=$USER_DB --password=$PW_DB $NAME_DB <<SQL
+            TRUNCATE RttMeasure;
+            TRUNCATE BandwidthMeasure;
+            DELETE FROM Test WHERE ID >= 1;
+            ALTER TABLE Test AUTO_INCREMENT = 1;
 
 SQL
-
-echo "DB cleared with success!"
+        echo "DB cleared with success!"
 
 EOF
+fi
