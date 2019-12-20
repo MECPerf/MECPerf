4b45325d28e655f3c57b2af76556b1053dd3fbb1
chiara.caiazza <chiara.caiazza@gmail.com>
2019-12-20 18:00:31 +0100

    add parameters

--- startAggregator.sh
+++ startAggregator.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp /usr/share/java/mysql.jar:Aggregator.jar it.unipi.dii.aggregator.Aggregator
+java -cp /usr/share/java/mysql.jar:/home/ubuntu/Aggregator.jar it.unipi.dii.aggregator.Aggregator --database-ip "" --database-name "" --database-user "" --database-password "" --aggregator-port 6766
