3b1578b594b8df243e1c661ddb4df0092cdb78dc
Valerio <valerio.luconi@gmail.com>
2020-02-12 16:56:07 +0100

    Refactor for using Parser and Sender threads

--- pcap-parser/src/main/java/it/cnr/iit/mecperf/pcap/Main.java
+++ pcap-parser/src/main/java/it/cnr/iit/mecperf/pcap/Main.java
@@ -1,58 +1,32 @@
 package it.cnr.iit.mecperf.pcap;
 
-import io.pkts.PacketHandler;
-import io.pkts.Pcap;
-import io.pkts.buffer.Buffer;
-import io.pkts.packet.Packet;
-import io.pkts.packet.TCPPacket;
-import io.pkts.packet.UDPPacket;
 import io.pkts.protocol.Protocol;
 import org.javatuples.Quintet;
 import org.javatuples.Pair;
+import org.tinylog.Logger;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        final Pcap pcap = Pcap.openStream("/home/valerio/tcpdump.pcap");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Logger.info("Program started");
+        BlockingQueue<String> toParse = new LinkedBlockingQueue<>();
+        BlockingQueue<String> toSend = new LinkedBlockingQueue<>();
         HashMap<Quintet<Protocol, String, String, Integer, Integer>, Flow> flows = new HashMap<>();
         Set<Pair<String, Integer>> servers = Collections.synchronizedSet(new HashSet<>());
         servers.add(new Pair<>("23.32.11.57", 443));
-
-        pcap.loop(new MECPerfPacketHandler(servers, flows));
-        for (Quintet<Protocol, String, String, Integer, Integer> fiveTuple : flows.keySet()) {
-            System.out.println(fiveTuple);
-            LinkedHashMap<Long, Integer> uplinkBytes = flows.get(fiveTuple).getBytes(Flow.DIR_UPLINK);
-            System.out.println("Uplink Bytes");
-            for (Long time : uplinkBytes.keySet()) {
-                System.out.printf("%d: %d\n", time, uplinkBytes.get(time));
-            }
-            LinkedHashMap<Long, Long> uplinkRtts = flows.get(fiveTuple).getRtts(Flow.DIR_UPLINK);
-            System.out.println("Uplink RTTs");
-            for (Long time : uplinkRtts.keySet()) {
-                System.out.printf("%d: %d\n", time, uplinkRtts.get(time));
-            }
-            System.out.println("Downlink Bytes");
-            LinkedHashMap<Long, Integer> downlinkBytes = flows.get(fiveTuple).getBytes(Flow.DIR_DOWNLINK);
-            boolean first = true;
-            long prevTime = 0;
-            for (Long time : downlinkBytes.keySet()) {
-                double bw;
-                if (first) {
-                    bw = 0;
-                    first = false;
-                } else {
-                    bw = ((double) downlinkBytes.get(time)) * 8 * 1000000 / (time - prevTime);
-                }
-                prevTime = time;
-                System.out.printf("%d: %d %f\n", time, downlinkBytes.get(time), bw);
-            }
-            LinkedHashMap<Long, Long> downlinkRtts = flows.get(fiveTuple).getRtts(Flow.DIR_DOWNLINK);
-            System.out.println("Dowlink RTTs");
-            for (Long time : downlinkRtts.keySet()) {
-                System.out.printf("%d: %d\n", time, downlinkRtts.get(time));
-            }
-        }
+        Parser parser = new Parser(toParse, toSend, servers);
+        Sender sender = new Sender(toSend);
+        parser.start();
+        sender.start();
+        Logger.info("Insert pcap file into toParse queue");
+        toParse.put("/home/valerio/tcpdump.pcap");
+        Logger.info("Inserted");
     }
 }
