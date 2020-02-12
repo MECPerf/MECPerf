77438c3a0e1906a0c2bd53940e9e5435da87947c
Valerio <valerio.luconi@gmail.com>
2020-02-12 16:55:38 +0100

    Add conversion of flow to json and send to Sender

--- pcap-parser/src/main/java/it/cnr/iit/mecperf/pcap/Parser.java
+++ pcap-parser/src/main/java/it/cnr/iit/mecperf/pcap/Parser.java
@@ -1,24 +1,27 @@
 package it.cnr.iit.mecperf.pcap;
 
+import com.google.gson.Gson;
 import io.pkts.Pcap;
 import io.pkts.protocol.Protocol;
 import org.javatuples.Pair;
 import org.javatuples.Quintet;
+import org.tinylog.Logger;
 
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Set;
 import java.util.concurrent.BlockingQueue;
 
 public class Parser extends Thread {
     private BlockingQueue<String> toParse;
+    private BlockingQueue<String> toSend;
     private MECPerfPacketHandler handler;
     private HashMap<Quintet<Protocol, String, String, Integer, Integer>, Flow> flows;
 
-    public Parser(BlockingQueue<String> toParse, Set<Pair<String, Integer>> servers) {
+    public Parser(BlockingQueue<String> toParse, BlockingQueue<String> toSend, Set<Pair<String, Integer>> servers) {
         super();
         this.toParse = toParse;
+        this.toSend = toSend;
         this.flows = new HashMap<>();
         this.handler = new MECPerfPacketHandler(servers, flows);
     }
@@ -26,19 +29,23 @@ public class Parser extends Thread {
     public void run() {
         while (true) {
             try {
+                Logger.info("Reading file name from queue");
                 String fileName = toParse.take();
                 Pcap pcap = Pcap.openStream(fileName);
                 pcap.loop(handler);
+                Gson gson = new Gson();
                 for (Quintet<Protocol, String, String, Integer, Integer> fiveTuple : flows.keySet()) {
-                    LinkedHashMap<Long, Integer> uplinkBytes = flows.get(fiveTuple).getBytes(Flow.DIR_UPLINK);
-                    LinkedHashMap<Long, Integer> downlinkBytes = flows.get(fiveTuple).getBytes(Flow.DIR_DOWNLINK);
-                    LinkedHashMap<Long, Long> uplinkRtts = flows.get(fiveTuple).getRtts(Flow.DIR_UPLINK);
-                    LinkedHashMap<Long, Long> downlinkRtts = flows.get(fiveTuple).getRtts(Flow.DIR_DOWNLINK);
-                    // TODO convert maps to JSONs with METADATA
-                    // TODO put JSONs in Senders queue
+                    // convert flow object to JSONs with METADATA
+                    String bandwidthJson = gson.toJson(flows.get(fiveTuple).toMeasurementResult(Flow.TYPE_BANDWIDTH));
+                    String rttJson = gson.toJson(flows.get(fiveTuple).toMeasurementResult(Flow.TYPE_RTT));
+                    // put JSONs in Senders queue
+                    toSend.put(bandwidthJson);
+                    if (rttJson != null)
+                        toSend.put(rttJson);
                 }
+                flows.clear();
             } catch (InterruptedException | IOException e) {
-                e.printStackTrace();
+                Logger.error(e);
             }
         }
     }
