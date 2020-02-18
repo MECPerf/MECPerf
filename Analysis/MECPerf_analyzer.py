4ff1be7d4f2df602a75515200e6dc4797de6f53d
chiara.caiazza <chiara.caiazza@gmail.com>
2020-02-18 18:28:22 +0100

    add folder

---
+++ Analysis/MECPerf_analyzer.py
@@ -0,0 +1,39 @@
+import json
+import ConfigParser
+import colors
+
+from AnalyzeMeasures import analyze_activebandwidthmeasures
+
+
+
+BASE_URL = ""
+BASE_PARAMS = {'compact':True, 'json':True, "likeKeyword":'experiment_active_'}
+        
+ 
+
+if __name__ ==  "__main__":
+    #read configuration file
+    config_parser = ConfigParser.RawConfigParser()
+    config_file_path = r'test1.conf'
+    config_parser.read(config_file_path)
+
+    BASE_URL = "http://" + str(config_parser.get("REST_server_params", "address")) + ":"
+    BASE_URL += str(config_parser.get("REST_server_params", "port"))
+
+    print "\n\n"
+    print "###########################################################################################"
+    print colors.BLUE + "EXPERIMENT CONF:"
+    print "BASE_URL: " + BASE_URL
+    print "dates: " + config_parser.get("experiment_params", "dates") 
+    print "from " + config_parser.get("experiment_params", "starting_time_intervals") + " [ " +\
+          config_parser.get("experiment_params", "duration_m") + " min.]" + colors.RESET
+    print "###########################################################################################"
+    print "\n\n"
+        
+    #active uplink/downlink bandwidth measures
+    analyze_activebandwidthmeasures(BASE_PARAMS, BASE_URL, 'Upstream', "TCPBandwidth", config_parser)
+    analyze_activebandwidthmeasures(BASE_PARAMS, BASE_URL, 'Downstream', "TCPBandwidth", config_parser)
+
+    analyze_activebandwidthmeasures(BASE_PARAMS, BASE_URL, 'Upstream', "UDPBandwidth", config_parser)
+    analyze_activebandwidthmeasures(BASE_PARAMS, BASE_URL, 'Downstream', "UDPBandwidth", config_parser)
+        
---
+++ Analysis/Measure.py
@@ -0,0 +1,5 @@
+class Measure:
+    def __init__(self,x, y, noise):
+        self.x = x
+        self.y = y
+        self.noise = noise
