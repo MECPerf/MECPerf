549111eaf64f924a1975d3f3e80e2f96e928ac8b
chiara.caiazza <chiara.caiazza@gmail.com>
2020-02-28 20:00:14 +0100

    removed ssh remote connections

--- buildDB.sh
+++ buildDB.sh
@@ -1,44 +1,25 @@
 #!/bin/bash
 
-if [[ $# -ne 6 ]]; then
-    echo "./buildDB sourceUser@sourceAddress sourcePassword destUsername destAddress destPassword  mysqlDestRootPassword"
+if [[ $# -ne 2 ]]; then
+    echo "./buildDB mysqlDestRootPassword DBUserNewPw"
     exit 1
 fi
 
-SOURCE_USER=$1
-SOURCE_PASSWORD=$2
-REMOTE_USERNAME=$3
-REMOTE_ADDRESS=$4
-REMOTE_PASSWORD=$5
-MYSQL_ROOT_PASSWORD=$6
+
+MYSQL_ROOT_PASSWORD=$1
+MECPerf_USER_NEWPW=$2
 
 USER_DB="MECPerf"
-PW_DB="password"
 DB_NAME="MECPerf"
 
-echo "sshpass -p $SOURCE_PASSWORD ssh $SOURCE_USER <<EOF"
-sshpass -p $SOURCE_PASSWORD ssh $SOURCE_USER <<EOF
-    echo "building DB..."
-    mysqldump --user=$USER_DB --password=$PW_DB --no-data $DB_NAME > MECPerf.sql
-    echo "Database schema create with no data"
-    echo "N.B: AUTO_INCREMENT in Test Table is not resetted"
-
-    echo "sshpass -p $REMOTE_PASSWORD scp MECPerf.sql $REMOTE_USERNAME@$REMOTE_ADDRESS:/home/$REMOTE_USERNAME"
-    sshpass -p $REMOTE_PASSWORD scp MECPerf.sql $REMOTE_USERNAME@$REMOTE_ADDRESS:/home/$REMOTE_USERNAME
-EOF
-
-echo "sshpass -p $REMOTE_PASSWORD ssh $REMOTE_USERNAME@$REMOTE_ADDRESS <<EOF"
-sshpass -p $REMOTE_PASSWORD ssh $REMOTE_USERNAME@$REMOTE_ADDRESS <<EOF
 echo "mysql --user=root --password=$MYSQL_ROOT_PASSWORD "
-mysql --user=root --password=$MYSQL_ROOT_PASSWORD <<EOF2
-CREATE USER IF NOT EXISTS '$USER_DB'@'localhost' IDENTIFIED BY '$PW_DB';
+mysql --user=root --password=$MYSQL_ROOT_PASSWORD <<EOF1
+CREATE USER IF NOT EXISTS '$USER_DB'@'localhost' IDENTIFIED BY '$MECPerf_USER_NEWPW';
 CREATE DATABASE  IF NOT EXISTS MECPerf;
 exit
-EOF2
+EOF1
 
-echo "importing MECPerf db"
-echo "mysql --user=root --password=$MYSQL_ROOT_PASSWORD MECPerf < MECPerf.sql"
-mysql --user=root --password=$MYSQL_ROOT_PASSWORD  MECPerf < MECPerf.sql
+mysql --user=root --password=$MYSQL_ROOT_PASSWORD  $DB_NAME < MECPerf.sql
 
 mysql --user=root --password=$MYSQL_ROOT_PASSWORD << EOF3
 USE MECPerf;
@@ -48,5 +29,3 @@ FLUSH PRIVILEGES;
 
 exit
 EOF3
-
-EOF
